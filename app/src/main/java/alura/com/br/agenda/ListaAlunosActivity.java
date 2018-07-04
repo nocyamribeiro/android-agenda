87b590ac85377cdbdf1f4e9e72687eb753f47bd9
nocyamribeiro <nocyamribeiro@gmail.com>
2018-07-03 22:51:57 -0300

    Primeiras aulas

--- app/src/main/java/alura/com/br/agenda/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/agenda/ListaAlunosActivity.java
@@ -2,6 +2,9 @@ package alura.com.br.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String [] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
